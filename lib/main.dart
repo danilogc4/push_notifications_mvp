0fce6698ca62343ca8cdd83003c5d8d1f4319052
danilogc4 <danilogc4@gmail.com>
2020-08-07 12:12:05 -0300

    push notification with workmanager

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:workmanager/workmanager.dart';
+import 'package:http/http.dart' as http;
+
+const periodicRequest = 'periodicRequest';
+const url = 'base_url_here';
+
+showNotification(
+    String message, FlutterLocalNotificationsPlugin localNotifications) async {
+  var android = AndroidNotificationDetails(
+      'channelId', 'channelName', 'channelDescription');
+  var iOS = IOSNotificationDetails();
+  var platform = NotificationDetails(android, iOS);
+  await localNotifications.show(0, 'Status atualizado!', '$message', platform,
+      payload: '$message');
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Workmanager.initialize(callbackDispatcher, isInDebugMode: true);
+  await Workmanager.registerPeriodicTask('teste', periodicRequest,
+      frequency: Duration(seconds: 10), initialDelay: Duration(seconds: 1));
   runApp(MyApp());
+}
+
+void callbackDispatcher() async {
+  Workmanager.executeTask((taskName, inputData) async {
+    FlutterLocalNotificationsPlugin localNotifications =
+        FlutterLocalNotificationsPlugin();
+    var android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iOS = IOSInitializationSettings();
+    var initSetttings = InitializationSettings(android, iOS);
+    localNotifications.initialize(initSetttings);
+
+    var resp = await http.get(url, headers: {});
+    var map = json.decode(resp.body);
+    if (map['status']) {
+      showNotification(map['message'], localNotifications);
+    }
+    print(map['status']);
+    return Future.value(true);
+  });
 }
 
 class MyApp extends StatelessWidget {
