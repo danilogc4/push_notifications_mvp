6c8e8b5a1571ddba618ebef4b9a8f4ad6e3ebce0
isterra <isterra00@gmail.com>
2020-08-09 19:30:17 -0300

    Add oneSignal

---
+++ lib/pages/home/bloc/bloc_home.dart
@@ -0,0 +1,11 @@
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+
+class BlocHome{
+
+  void initOneSignal(){
+    OneSignal.shared.init("bc2472d1-fac8-433f-be56-48ea9ecbc1ef");
+    OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.none);
+  }
+
+
+}
