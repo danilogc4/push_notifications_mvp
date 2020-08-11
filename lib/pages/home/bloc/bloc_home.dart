bc024239c80673cd891242c2269e6a929a991f13
isterra <isterra00@gmail.com>
2020-08-11 14:43:33 -0300

    Remove externalUser

--- lib/pages/home/bloc/bloc_home.dart
+++ lib/pages/home/bloc/bloc_home.dart
@@ -6,8 +6,12 @@ class BlocHome{
     OneSignal.shared.init("4c5b5d9f-fbd9-4620-bed8-62ec6760ef13");
     OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.none);
   }
+  //Ao logar
 void load()async{
     OneSignal.shared.setExternalUserId("ISTERRA2");
   }
-
+  //Ao deslogar
+void dispose(){
+  OneSignal.shared.removeExternalUserId();
+}
 }
